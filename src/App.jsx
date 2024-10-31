eb7b19ff40b4b9445e3eaf4cd13bd1c3e204fd51
rishtansoft <rishtansoft@gmail.com>
2024-10-31 08:47:32 +0500

    feat: folders create, main architecture

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,51 @@
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import { createContext, useEffect, useState } from 'react';
+import MainLayout from './layouts/MainLayout';
+import ErrorPage from './pages/ErrorPage';
+
+export const ThemeContext = createContext();
+
 function App() {
+  const [theme, setTheme] = useState('light');
+  const [token, setToken] = useState(localStorage.getItem('token'));
+
+  const navigate = useNavigate();
+
+  function PrivateRoute({ isAuth, children }) {
+    if (!isAuth) {
+      navigate('/login');
+    }
+
+    return children;
+  }
+
+  useEffect(() => {
+    if (localStorage.getItem('token')) {
+      setToken(localStorage.getItem('token'))
+    } else {
+      navigate('/login')
+    }
+  }, [navigate])
+
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <PrivateRoute isAuth={!!token}>
+              <MainLayout>
+                <Home></Home>
+              </MainLayout>
+            </PrivateRoute>
+          }
+        ></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='*' element={<ErrorPage></ErrorPage>}></Route>
+      </Routes>
+    </ThemeContext.Provider>
   );
 }
 
